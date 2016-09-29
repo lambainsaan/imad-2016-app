e977263816dfbbffaefc83bad77ecb19e6107890
Rhitik Bhatt <bhattrhitik95@gmail.com>
2016-09-29 10:57:46 +0530

    Added some html files

--- server.js
+++ server.js
@@ -5,34 +5,33 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/articleone', function(req, res){
-    res.send("Just having fun with JS");
-} )
+app.get('/articleone', function(req, res) {
+    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+})
 
 
-app.get('/articletwo', function(req, res){
-    res.send("Just having fun with JS 2");
-} )
+app.get('/articletwo', function(req, res) {
+    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
+})
 
+app.get('/articlethree', function(req, res) {
+    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+})
 
-app.get('/articlethree', function(req, res){
-    res.send("Just having fun with JS3");
-} )
-
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+app.get('/ui/style.css', function(req, res) {
+    res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+app.get('/ui/madi.png', function(req, res) {
+    res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
-app.listen(8080, function () {
-  console.log(`IMAD course app listening on port ${port}!`);
+app.listen(8080, function() {
+    console.log(`IMAD course app listening on port ${port}!`);
 });
