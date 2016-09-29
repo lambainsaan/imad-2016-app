e139e7d5b33fe0aa3c8d687d2efb5a916b1d1616
Rhitik Bhatt <bhattrhitik95@gmail.com>
2016-09-29 12:51:50 +0530

    Added some data in to the server.js file

--- server.js
+++ server.js
@@ -5,22 +5,55 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+content = {
+    'article-one': {
+        'title': 'Article One',
+        'heading': 'Article One',
+        'date': 'Aug-20-2001',
+        'content': `
+          <p>
+            Finally article one is live, you get to learn so much from article one.
+            I am going to let you know what is article one all about, so let's get
+            started bros and guys! :)
+          </p>
+          <i>Hey this is italic <u>alright?</u></i>
+          <h2>This is the best you could do with this shit and this is the best you could do with this shit and I don't know what the hell I am talking about.</h2>
+        `,
+
+    }
+}
+
+function contentTemplate(data){
+  var title = data.title;
+  var heading = data.heading;
+  var date = data.date;
+  var content = data.content;
+  var htmlTemplate = `
+  <!DOCTYPE html>
+  <html>
+    <head>
+      <meta charset="utf-8">
+      <title>${title}</title>
+    </head>
+    <body>
+    <h1>${heading}</h1>
+    <p>${date}</p>
+      ${content}
+    </body>
+  </html>
+
+  `
+  return htmlTemplate;
+}
+
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/articleone', function(req, res) {
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
-})
 
-
-app.get('/articletwo', function(req, res) {
-    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-})
-
-app.get('/articlethree', function(req, res) {
-    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
-})
+app.get('/:articleName', function(req, res) {
+    res.send(contentTemplate(content[req.params.articleName]))
+});
 
 app.get('/ui/style.css', function(req, res) {
     res.sendFile(path.join(__dirname, 'ui', 'style.css'));
